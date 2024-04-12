bc0f2ab628b5db3b0080a5571b2c237fc1f5eadd
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-04-11 21:12:11 -0700

    Fix DoctorFactory not working

--- database/factories/DoctorFactory.php
+++ database/factories/DoctorFactory.php
@@ -17,7 +17,13 @@ class DoctorFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'lastname' => fake()->lastName(),
+            'speciality' => fake()->randomElement(['cardiology', 'orthopedics', 'oncology', 'neurology', 'pediatrics']),
+            'admission_date' => fake()->date(),
+            'professional_id' => fake()->uuid(),
+            'phone_number' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
         ];
     }
 }
