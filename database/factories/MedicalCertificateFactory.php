5f9717084c988e8d526fef6708e4064193167bbf
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-04-12 02:11:36 -0700

    Update MedicalCertificateFactory.php

--- database/factories/MedicalCertificateFactory.php
+++ database/factories/MedicalCertificateFactory.php
@@ -17,7 +17,14 @@ class MedicalCertificateFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'date' => fake()->date(),
+            'age' => fake()->numberBetween(1, 100),
+            'height' => fake()->numberBetween(145, 200),
+            'weight' => fake()->randomFloat(2, 50, 120),
+            'systolic_pressure' => fake()->numberBetween(50, 120),
+            'diastolic_pressure' => fake()->numberBetween(60, 80),
+            'hearth_rate' => fake()->numberBetween(60, 120),
+            'respiratory_rate' => fake()->numberBetween(12, 18),
         ];
     }
 }
