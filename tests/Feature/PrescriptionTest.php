9228af25e8c776c094db172f4698c0a5822da4af
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-04-17 19:40:38 -0700

    Refactor PrescriptionController to support multiple medicines in prescriptions

---
+++ tests/Feature/PrescriptionTest.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Doctor;
+use App\Models\Medicine;
+use App\Models\Patient;
+use App\Models\MedicalRecord;
+use App\Models\Prescription;
+
+class PrescriptionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_prescriptions_can_be_retrieved()
+    {
+        $response = $this->get('/api/prescriptions');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_prescription_can_be_created()
+    {
+        $medicineA = Medicine::factory()->create();
+        $medicineB = Medicine::factory()->create();
+
+        $doctor = Doctor::factory()->create();
+        $patient = Patient::factory()->create();
+
+        $medicalRecord = MedicalRecord::factory()->create([
+            'doctor_id' => $doctor->id,
+            'patient_id' => $patient->id,
+        ]);
+
+        $response = $this->post('/api/prescriptions', [
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+            'medicines' => [
+                [
+                    "medicine_id" => $medicineA->id,
+                    "indications" => "Before meals, 3 times a day, for 5 days"
+                ],
+                [
+                    "medicine_id" => $medicineB->id,
+                    "indications" => "After meals, 2 times a day, for 3 days"
+                ]
+            ]
+        ]);
+
+        $response->assertStatus(201);
+
+        $response->assertJson([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+            'medicines' => [
+                [
+                    'id' => $response['medicines'][0]['id'],
+                    'name' => $medicineA->name,
+                    'pivot' => [
+                        'indications' => 'Before meals, 3 times a day, for 5 days',
+                    ],
+                ],
+                [
+                    'id' => $response['medicines'][1]['id'],
+                    'name' => $medicineB->name,
+                    'pivot' => [
+                        'indications' => 'After meals, 2 times a day, for 3 days',
+                    ],
+                ],
+            ]
+        ]);
+
+        $this->assertDatabaseHas('prescriptions', [
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+        ]);
+
+        $this->assertDatabaseHas('medicine_prescription', [
+            'medicine_id' => $medicineA->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'Before meals, 3 times a day, for 5 days',
+        ]);
+
+        $this->assertDatabaseHas('medicine_prescription', [
+            'medicine_id' => $medicineB->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'After meals, 2 times a day, for 3 days',
+        ]);
+    }
+
+    public function test_prescription_can_be_retrieved_by_id()
+    {
+        $medicineA = Medicine::factory()->create();
+        $medicineB = Medicine::factory()->create();
+
+        $doctor = Doctor::factory()->create();
+        $patient = Patient::factory()->create();
+
+        $medicalRecord = MedicalRecord::factory()->create([
+            'doctor_id' => $doctor->id,
+            'patient_id' => $patient->id,
+        ]);
+
+        $prescription = Prescription::factory()->create([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+        ]);
+
+        $prescription->medicines()->attach([
+            $medicineA->id => ['indications' => 'Before meals, 3 times a day, for 5 days'],
+            $medicineB->id => ['indications' => 'After meals, 2 times a day, for 3 days'],
+        ]);
+
+        $response = $this->get("/api/prescriptions/{$prescription->id}");
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+            'medicines' => [
+                [
+                    'id' => $medicineA->id,
+                    'name' => $medicineA->name,
+                    'pivot' => [
+                        'indications' => 'Before meals, 3 times a day, for 5 days',
+                    ],
+                ],
+                [
+                    'id' => $medicineB->id,
+                    'name' => $medicineB->name,
+                    'pivot' => [
+                        'indications' => 'After meals, 2 times a day, for 3 days',
+                    ],
+                ],
+            ]
+        ]);
+    }
+    
+    public function test_prescription_can_be_updated()
+    {
+        $medicineA = Medicine::factory()->create();
+        $medicineB = Medicine::factory()->create();
+
+        $doctor = Doctor::factory()->create();
+        $patient = Patient::factory()->create();
+
+        $medicalRecord = MedicalRecord::factory()->create([
+            'doctor_id' => $doctor->id,
+            'patient_id' => $patient->id,
+        ]);
+
+        $prescription = Prescription::factory()->create([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+        ]);
+
+        $prescription->medicines()->attach([
+            $medicineA->id => ['indications' => 'Before meals, 3 times a day, for 5 days'],
+            $medicineB->id => ['indications' => 'After meals, 2 times a day, for 3 days'],
+        ]);
+
+        $response = $this->put("/api/prescriptions/{$prescription->id}", [
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed, updated',
+            'date' => '2024-04-12',
+            'medicines' => [
+                [
+                    "medicine_id" => $medicineA->id,
+                    "indications" => "Before meals, 3 times a day, for 5 days, updated"
+                ],
+                [
+                    "medicine_id" => $medicineB->id,
+                    "indications" => "After meals, 2 times a day, for 3 days, updated"
+                ]
+            ]
+        ]);
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed, updated',
+            'date' => '2024-04-12',
+            'medicines' => [
+                [
+                    'id' => $medicineA->id,
+                    'name' => $medicineA->name,
+                    'pivot' => [
+                        'indications' => 'Before meals, 3 times a day, for 5 days, updated',
+                    ],
+                ],
+                [
+                    'id' => $medicineB->id,
+                    'name' => $medicineB->name,
+                    'pivot' => [
+                        'indications' => 'After meals, 2 times a day, for 3 days, updated',
+                    ],
+                ],
+            ]
+        ]);
+
+        $this->assertDatabaseHas('prescriptions', [
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed, updated',
+            'date' => '2024-04-12',
+        ]);
+
+        $this->assertDatabaseHas('medicine_prescription', [
+            'medicine_id' => $medicineA->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'Before meals, 3 times a day, for 5 days, updated',
+        ]);
+
+        $this->assertDatabaseHas('medicine_prescription', [
+            'medicine_id' => $medicineB->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'After meals, 2 times a day, for 3 days, updated',
+        ]);
+
+        $this->assertDatabaseMissing('medicine_prescription', [
+            'medicine_id' => $medicineA->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'Before meals, 3 times a day, for 5 days',
+        ]);
+
+        $this->assertDatabaseMissing('medicine_prescription', [
+            'medicine_id' => $medicineB->id,
+            'prescription_id' => $response['id'],
+            'indications' => 'After meals, 2 times a day, for 3 days',
+        ]);
+    }
+
+    public function test_prescription_can_be_deleted()
+    {
+        $medicineA = Medicine::factory()->create();
+        $medicineB = Medicine::factory()->create();
+
+        $doctor = Doctor::factory()->create();
+        $patient = Patient::factory()->create();
+
+        $medicalRecord = MedicalRecord::factory()->create([
+            'doctor_id' => $doctor->id,
+            'patient_id' => $patient->id,
+        ]);
+
+        $prescription = Prescription::factory()->create([
+            'medical_record_id' => $medicalRecord->id,
+            'notes' => 'Take the following medicines as prescribed',
+            'date' => '2024-04-11',
+        ]);
+
+        $prescription->medicines()->attach([
+            $medicineA->id => ['indications' => 'Before meals, 3 times a day, for 5 days'],
+            $medicineB->id => ['indications' => 'After meals, 2 times a day, for 3 days'],
+        ]);
+
+        $response = $this->delete("/api/prescriptions/{$prescription->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('prescriptions', [
+            'id' => $prescription->id,
+        ]);
+
+        $this->assertDatabaseMissing('medicine_prescription', [
+            'prescription_id' => $prescription->id,
+        ]);
+    }
+}
