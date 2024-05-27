ab0536c72914035bca25ba269bc666ee35d1f64b
Kenneth Quintero <56099614+cin4ed@users.noreply.github.com>
2024-05-26 17:54:26 -0700

    Add routes authentication

--- routes/api.php
+++ routes/api.php
@@ -25,11 +25,14 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('users', UserController::class);
-Route::apiResource('doctors', DoctorController::class);
-Route::apiResource('patients', PatientController::class);
-Route::apiResource('medicines', MedicineController::class);
-Route::apiResource('medical-records', MedicalRecordController::class);
-Route::apiResource('prescriptions', PrescriptionController::class);
-Route::apiResource('medical-certificates', MedicalCertificateController::class);
-Route::get('medical-certificates/{medical_certificate}/generate-pdf', [MedicalCertificateController::class, 'generatePdf']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::apiResource('users', UserController::class);
+    Route::apiResource('doctors', DoctorController::class);
+    Route::apiResource('patients', PatientController::class);
+    Route::apiResource('medicines', MedicineController::class);
+    Route::apiResource('medical-records', MedicalRecordController::class);
+    Route::apiResource('prescriptions', PrescriptionController::class);
+    Route::apiResource('medical-certificates', MedicalCertificateController::class);
+    Route::get('medical-certificates/{medical_certificate}/generate-pdf', [MedicalCertificateController::class, 'generatePdf']);
+});
+
